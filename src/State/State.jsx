bcbf62bbf14daf4b04e997e7c383b9d142aa3ab2
MAnwarulhaq <muhammadanwarulhaq15@gmail.com>
2025-06-14 11:00:02 +0500

    Correct Way to Manage the state

--- src/State/State.jsx
+++ src/State/State.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
+import { useState } from 'react';
 
+
+
+// State is a way to manage data that can change over time in a React component.
 
 // Wrong way to manage state in React
+// const State = () => {
+//     let fruits = "Apple";
+//     const changeFruits = () => {
+//         fruits = "Banana";
+//         console.log(fruits);
+//     }
+//   return (
+//     <div>
+//         <h1>What is State Management</h1>
+//         <h1>{fruits}</h1>
+//         <button onClick={changeFruits}>Change The fruit name</button>
+//     </div>
+//   )
+// }
 
+// Correct way to manage state in React
 const State = () => {
-    let fruits = "Apple";
+    const [fruits, setFruits] = useState("Apple");
     const changeFruits = () => {
-        fruits = "Banana";
+        setFruits("Banana");
         console.log(fruits);
     }
   return (
