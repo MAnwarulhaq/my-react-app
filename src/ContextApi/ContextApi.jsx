f600783b52c4fb8d7c6b772a52a7b3b9963973aa
MAnwarulhaq <muhammadanwarulhaq15@gmail.com>
2025-07-07 12:37:23 +0500

    Context Api

--- src/ContextApi/ContextApi.jsx
+++ src/ContextApi/ContextApi.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CollegeInfo from './CollegeInfo'
+import { SubjectData } from './CollegeData'
 const ContextApi = () => {
+  const [subjectInfo,setSubjectInfo]=useState("")
   return (
     <div style={{backgroundColor:"orange", padding:"20px"}}>
-        <h1>Context Api</h1>
+        <SubjectData.Provider value={subjectInfo}>
+          <select value={subjectInfo} name="" id="" onChange={(e)=>setSubjectInfo(e.target.value)}>
+            <option value="">Select Subject</option>
+            <option value="Math">MAth</option>
+            <option value="English">English</option>
+            <option value="urdu">Urdu</option>
+            <option value="Islamiat">Islamiat</option>
+          </select>
+          <h1>Context Api</h1>
+          <button onClick={()=>setSubjectInfo("")}>Clear Subject</button>
         <CollegeInfo/>
+        </SubjectData.Provider>
     </div>
   )
 }
--- src/ContextApi/Subject.jsx
+++ src/ContextApi/Subject.jsx
@@ -6,7 +6,7 @@ const Subject = () => {
     const subject=useContext(SubjectData)
   return (
     <div  style={{backgroundColor:"brown", padding:"20px"}}>
-        <h1>Subject</h1>
+        <h1>Subject:{subject}</h1>
     </div>
   )
 }
