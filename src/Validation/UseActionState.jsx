ebb5d9d563aab3a1cd2d5c169aa245c3083b9c38
MAnwarulhaq <muhammadanwarulhaq15@gmail.com>
2025-07-20 17:05:39 +0500

    useActionStat

--- src/Validation/UseActionState.jsx
+++ src/Validation/UseActionState.jsx
@@ -1,19 +1,34 @@
 import React from 'react'
 
 const UseActionState = () => {
-    const handleLogin = () => {
-        
+    const handleLogin = (preData,formData) => {
+        let name= formData.get('name');
+        let password = formData.get('password');
+        console.log("Name: ", name);
+        console.log("Password: ", password);
+        const regex = /^[A-Z0-9]+$/i;
+        if (!regex.test(password)) {
+            return {console:"Password must contain at least one uppercase letter and one number"};
+        }
+        else if (name.length > 5) {
+          return {console:"Name should not be more than 5 characters long"};
+          }
+        }
+
+
     }
 
-    const [] = React.useActionState(handleLogin)
+    const [data,action,pending] = React.useActionState(handleLogin)
   return (
     <div>
         <h1>Use Action State Example</h1>
-        <input type="text" name="" id="" />
+        <form action={action}>
+        <input type="text" name="name" id="" />
         <br /><br />
-        <input type="text" name="" id="" />
+        <input type="text" name="password" id="" />
         <br /><br />
         <button>Submit</button>
+        </form>
     </div>
   )
 }
