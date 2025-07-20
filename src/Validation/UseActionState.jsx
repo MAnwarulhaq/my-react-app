4bf32bdf29a57fb3d2486f6bdf8f9f28165fefc5
MAnwarulhaq <muhammadanwarulhaq15@gmail.com>
2025-07-20 17:29:39 +0500

    code update

--- src/Validation/UseActionState.jsx
+++ src/Validation/UseActionState.jsx
@@ -4,28 +4,40 @@ const UseActionState = () => {
     const handleLogin = (preData,formData) => {
         let name= formData.get('name');
         let password = formData.get('password');
-        console.log("Name: ", name);
-        console.log("Password: ", password);
+        // console.log("Name: ", name);
+        // console.log("Password: ", password);
         const regex = /^[A-Z0-9]+$/i;
-        if (!regex.test(password)) {
-            return {console:"Password must contain at least one uppercase letter and one number"};
+        if (!name || name.length > 6){
+           return {error:"Name should be not empty and should not be more than 6 characters long",name,password};
         }
-        else if (name.length > 5) {
-          return {console:"Name should not be more than 5 characters long"};
+        else if  (!regex.test(password)) {
+          
+           return {error:"Password must contain at least one uppercase letter and one number" ,name,password};
           }
+        else{
+            return {message:"Login successful",name,password} 
         }
 
 
+        
     }
+  
 
     const [data,action,pending] = React.useActionState(handleLogin)
+    //   console.log(data)
   return (
     <div>
         <h1>Use Action State Example</h1>
+        {
+            data?.message && <span style={{color: "green"}}>{data.message}</span>
+        }
+         {
+            data?.error && <span style={{color: "red"}}>{data.error}</span> 
+}
         <form action={action}>
-        <input type="text" name="name" id="" />
+        <input type="text" name="name" id="" defaultValue={data?.name}/>
         <br /><br />
-        <input type="text" name="password" id="" />
+        <input type="text" name="password" id="" defaultValue={data?.password}/>
         <br /><br />
         <button>Submit</button>
         </form>
