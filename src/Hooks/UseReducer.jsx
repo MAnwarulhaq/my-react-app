3dc7668b2b560c659fc0c4e598fd19f085d91668
MAnwarulhaq <muhammadanwarulhaq15@gmail.com>
2025-07-22 14:30:29 +0500

    UseReducer

--- src/Hooks/UseReducer.jsx
+++ src/Hooks/UseReducer.jsx
@@ -1,15 +1,28 @@
+import { type } from '@testing-library/user-event/dist/type';
 import React from 'react'
 
+ const emptyData = {
+        name: "",
+        email: "",
+        age: "",
+        city: "",
+        password: ""
+    }
+const reducer = (state, action) => {
+    console.log("Reducer called with action:", action);
+}
+
 const UseReducer = () => {
+    const [state, dispatch] = React.useReducer(reducer,emptyData)
+   
   return (
     <div>
         <h1>useReducer</h1>
-        <input type="text" name="" id="" placeholder='Enter your name' /><br /><br />
-        <input type="text" name="" id="" placeholder='Enter your email'/><br /><br />
-        <input type="text" name="" id="" placeholder='Enter your age' /><br /><br />
-        <input type="text" name="" id="" placeholder='Enter your city name'/><br /><br />
-        <input type="text" name="" id="" placeholder='Enter your address'/><br /><br />
-        <input type="text" name="" id="" placeholder='Enter yuor password'/><br /><br />
+        <input type="text" name="" id="" placeholder='Enter your name' onChange={(event)=>dispatch({val:event.target.value,type:"name"})} /><br /><br />
+        <input type="text" name="" id="" placeholder='Enter your email' onChange={(event)=>dispatch({val:event.target.value,type:"email"})}/><br /><br />
+        <input type="text" name="" id="" placeholder='Enter your age'  onChange={(event)=>dispatch({val:event.target.value,type:"age"})}/><br /><br />
+        <input type="text" name="" id="" placeholder='Enter your city name'  onChange={(event)=>dispatch({val:event.target.value,type:"city"})}/><br /><br />
+        <input type="text" name="" id="" placeholder='Enter yuor password' onChange={(event)=>dispatch({val:event.target.value,type:"password"})}/><br /><br />
         <button>Submit</button>
     </div>
   )
