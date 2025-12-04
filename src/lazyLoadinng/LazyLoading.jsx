b5fd0aa87992aef4ae4b4178bbed9766228687a3
MAnwarulhaq <muhammadanwarulhaq15@gmail.com>
2025-12-04 20:28:13 +0500

    update the code

--- src/lazyLoadinng/LazyLoading.jsx
+++ src/lazyLoadinng/LazyLoading.jsx
@@ -1,14 +1,18 @@
-import React, { useState } from 'react'
-import Products from './Products'
+import React, { useState,lazy,Suspense } from 'react'
+// import Products from './Products'
+
+const Products = lazy(()=> import('./Products'))
 
 const LazyLoading = () => {
     const [open,setOpen] = useState(false)
   return (
     <div>
         <button onClick={()=>setOpen(!open)}>see products</button>
+        <Suspense fallback={<h1>...loading</h1>}>
         {
             open &&  <Products/> 
         }
+        </Suspense>
         
     </div>
   )
