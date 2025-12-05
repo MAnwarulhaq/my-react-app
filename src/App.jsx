de703150925e1e3a0ed05bd57174533fc3682348
MAnwarulhaq <muhammadanwarulhaq15@gmail.com>
2025-12-05 17:14:25 +0500

    Update the code

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@
 // import ContextApi from "./ContextApi/ContextApi";
 // import UseRef from './Hooks/UseRef'
 // import LoginValidation from "./Validation/LoginValidation";
-// import UseMemo from "./Hooks/UseMemo";
+import UseMemo from "./Hooks/UseMemo";
 // import Parent from "./Hooks/usecallback/Parent";
 // import RadioandDropdown from "./RadioandDropdownButton/RadioandDropdown";
 // import EnhancedProfile from "./HOC/EnhancedProfile";
@@ -20,11 +20,11 @@ function App() {
       {/* <ContextApi/> */}
       {/* <UseRef/> */}
       {/* <LoginValidation/> */}
-      {/* <UseMemo/> */}
+      <UseMemo/>
       {/* <Parent/> */}
       {/* < RadioandDropdown /> */}
       {/* <EnhancedProfile name="Anwar"/> */}
-      <LazyLoading/>
+      {/* <LazyLoading/> */}
 
       
     </>
--- src/Hooks/UseMemo.jsx
+++ src/Hooks/UseMemo.jsx
@@ -1,21 +1,40 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 
 const UseMemo = () => {
   const [count, setCount] = React.useState(0)
   const [item, setItem] = React.useState(10)
+  const [total ,setTotal ] =useState()
 
   // function dono button p call ho rha h es ko rokny k liye useMemo use krty h
   // heavy calulation k liye use hota h
 
-  //     function multiCount  (){
-  //         console.log("MultiCount called")
-  // return count*5
-  //     }
-  const multiCount = React.useMemo(
-    () => {
+  // const  multiCount =() =>{
+  //   console.log("MultiCount called")
+  //   let total = 0
+  //   for (let i = 0; i < 1000000000; i++) {
+  //     total += i
+  //   }
+  //   return  total
+  // }
+
+  // const result = multiCount()
+  const  multiCount =(count) =>{
     console.log("MultiCount called")
-    return count * 5
-  }, [count]) // jab count change hoga tabhi multiCount call hoga
+    let total = 0
+    for (let i = 0; i < 1000000000; i++) {
+      total += i
+    }
+    return  total + count
+  }
+
+  const result =useMemo(()=>  multiCount(count),[count])
+
+  // setTotal(result)
+  // const multiCount = React.useMemo(
+  //   () => {
+  //   console.log("MultiCount called")
+  //   return count * 5
+  // }, [count]) // jab count change hoga tabhi multiCount call hoga
 
 
 
@@ -23,10 +42,11 @@ const UseMemo = () => {
     <div>
 
       <h1>useMemo</h1>
-      <h2>Count: {count}</h2>
+      <h2>total: {result}</h2>
+      <h3>{count}</h3>
       <h2>Item: {item}</h2>
       {/* <h1>{multiCount()}</h1> */}
-      <h1>{multiCount}</h1>
+      {/* <h1>{multiCount}</h1> */}
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <button onClick={() => setItem(item * 10)}>Item</button>
     </div>
