2eda66117d17f8c77fb0bb763df2b79a84136aca
MAnwarulhaq <muhammadanwarulhaq15@gmail.com>
2025-12-06 13:47:51 +0500

    Update the code

--- src/RTK/components/Products.jsx
+++ src/RTK/components/Products.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-import { useGetProductsQuery } from '../service/products'
+import { useGetProductsQuery ,useGetProductQuery} from '../service/products'
 const Products = () => {
-    const { data } = useGetProductsQuery()
-    console.log(data?.products)
+    const id =10
+    // get all products
+    // const { data } = useGetProductsQuery()
+    // console.log(data?.products)
+
+    // get products by id
+    const { data } = useGetProductQuery(id)
+    console.log(data,"Products")
+
     return (
         <div>
             <h1>Products</h1>
